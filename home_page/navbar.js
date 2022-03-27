18e94d6f58317b78c4f0f61a378cb79b3dcd4b18
erikaas24 <94107883+erikaas24@users.noreply.github.com>
2022-03-27 14:30:06 +0200

    Delete home_page directory
    Sletting

--- home_page/navbar.js
+++
@@ -1,6 +0,0 @@
-const ToggleButton = document.querySelector("#toggle_button")
-const NavbarRight = document.querySelector(".navbar_right")
-
-ToggleButton.addEventListener('click',() => {
-    NavbarRight.classList.toggle('active')
-})
